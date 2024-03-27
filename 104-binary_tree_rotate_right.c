4a05690461ff3e3f1e8bb112bf6372784b104c39
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-03-27 22:17:07 +0000

    task 23

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,45 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_right - perform rotate left operation
+ *@tree: the tree root to be rotated
+ * Return: new tree root.
+ */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *granny, *oldtop, *oldleft, *temp;
+
+	if (tree == NULL)
+		return (NULL);
+
+	granny = tree->parent;
+	oldtop = tree;
+	oldleft = tree->left;
+	temp = NULL;
+
+	/*upgrade the oldright to the position of parent */
+	if (granny != NULL && oldleft != NULL)
+	{
+		if (granny->left == oldtop)
+			granny->left = oldleft;
+		else if (granny->right == oldtop)
+			granny->right = oldleft;
+
+		oldleft->parent = granny;
+	}
+	else if (granny == NULL)
+		oldleft->parent = NULL;
+	else
+		return (tree);
+
+	/*The right child on oldleft should be moved to the right of oldtop*/
+	if (oldleft->right != NULL)
+		temp = oldleft->right;
+	oldleft->right = oldtop;
+	oldtop->left = temp;
+
+	if (temp != NULL)
+		temp->parent = oldleft;
+	oldtop->parent = oldleft;
+	return (oldleft);
+}
